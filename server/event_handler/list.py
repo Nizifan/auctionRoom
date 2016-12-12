9ebc75521f874650d80822fd261851338f80b2cb
zifan <zifan@ZZdeMac-mini.local>
2016-12-12 17:26:55 +0800

    implement all necessary function without test

---
+++ server/event_handler/list.py
@@ -0,0 +1,28 @@
+from common.message import MessageType
+from server.memory import socket_mappings, room_mappings
+from server.memory import user_id_mappings
+from server.broadcast import broadcast
+
+
+
+def run(sc, parameter):
+    message_ = {
+        "auctionname":{},
+        "bid":{},
+        "userlist":[]
+    }
+
+    room = socket_mappings["room"][sc]
+
+    message_["auctionname"][room] = room_mappings["auctionname"][room]
+    message_["bid"][room] = room_mappings["bid"][room]
+    nickname_ = []
+    for id in room_mappings["user_id"][room]:
+        nickname_.append(user_id_mappings["nickname"][id])
+    message_["userlist"][room] = nickname_
+
+    message = {"message": message_}
+    broadcast(MessageType.leave,message)
+
+
+
